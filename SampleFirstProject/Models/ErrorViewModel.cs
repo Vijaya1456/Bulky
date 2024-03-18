bf317bce361547ba74b91c1f99cd6c95200cfad9
koteswararao <koteswararao@KOTESWARARAO>
2024-03-18 11:14:17 -0400

    Add project files.

---
+++ SampleFirstProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleFirstProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
