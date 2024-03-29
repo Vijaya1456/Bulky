98eecf434d496aba5bc3a5bf863e62ed7bc7a5a5
koteswararao <koteswararao@KOTESWARARAO>
2024-03-29 07:39:18 -0400

    Initial Commit

---
+++ SampleFirstProject/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleFirstProject.Data;
+using SampleFirstProject.Models;
+
+namespace SampleFirstProject.Controllers
+{
+    public class CategoryController : Controller
+
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;       
+        }
+        public IActionResult Index()
+        {
+            List<Category> ObjCategoryList = _db.Categories.ToList();
+            return View(ObjCategoryList);
+        }
+    }
+}
